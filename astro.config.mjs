32ee69d8fd601ec02675a302df6819721bee970a
Robert <yutuan.victory@gmail.com>
2025-11-04 13:57:11 +0800

    feat: add new dependencies for math and accessibility support
    - Added rehype-accessible-emojis, rehype-katex, remark-math, and remark-toc to package.json for enhanced content rendering.
    - Updated main layout to include KaTeX stylesheet for math rendering.
    - Increased max-width for post layout to improve readability.
    - Added a new post on compilation principles with detailed notes and structured content.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import remarkToc from "remark-toc";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath, [remarkToc, { heading: "toc", maxDepth: 5 }]],
+    rehypePlugins: [rehypeKatex, rehypeAccessibleEmojis],
+  },
 });
